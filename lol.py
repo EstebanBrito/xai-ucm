e2aef8933480f82e1766e64c5c31f8a8025a4289
EstebanBrito <esteban.brito.borges@gmail.com>
2021-04-14 20:34:03 -0500

    Improved a few little things inside GUI

--- lol.py
+++ lol.py
@@ -28,7 +28,7 @@ class LabelerApp():
         kwargs = {}
         if window is ImageViewerWindow:
             if len(self.img_names)!=0:
-                if self.count <= len(self.img_names):
+                if self.count < len(self.img_names):
                     kwargs = {
                         'image_name': self.img_names[self.count],
                         'image_number': self.count + 1,
@@ -58,6 +58,7 @@ class WelcomeWindow(tk.Tk):
         # Window options
         self.title('Proyecto XAI-UCM: Votación de Imágenes')
         self.geometry(f'{self.w}x{self.h}')
+        self.resizable(False, False)
         # Windows text
         title = tk.Label(self, text='VOTACIÓN DE IMÁGENES. PROYECTO XAI-UCM')
         title.place(x=0, y=30, width=self.w)
@@ -77,7 +78,7 @@ class WelcomeWindow(tk.Tk):
 class ImageViewerWindow(tk.Tk):
     main_img_size, normal_img_size = 200, 150
     space_between = 50
-    w = 4*normal_img_size + 5*space_between
+    w = 850 # 4*normal_img_size + 5*space_between
     h = 600
     # References to imgs are needed for tkinter does not hold references
     main_img = None
@@ -91,44 +92,47 @@ class ImageViewerWindow(tk.Tk):
         super().__init__()
         self.geometry(f'{self.w}x{self.h}')
         self.title(f'Imagen {image_number}/{no_of_images}')
+        self.resizable(False, False)
         # Main img and instructions
-        self.load_images_widgets('Giant_Panda_2.jpeg')
+        self.load_images_widgets(image_name)
 
         main_lbl = tk.Label(self, image=self.main_img)
-        main_lbl.place(x=350, y=10, width=self.main_img_size, height=self.main_img_size)
-        text = tk.Label(self, text='¿Cuál de las imágenes de abajo explica de mejor manera la imagen de arriba? Presiona el botón debajo de la imagen de tu elección')
-        text.place(x=0, y=320, width=self.w)
+        main_lbl.place(x=325, y=10, width=self.main_img_size, height=self.main_img_size)
+        text = tk.Label(self, text='¿Cuál de las imágenes de abajo explica de mejor manera la imagen de arriba?\nPresiona el botón debajo de la imagen de tu elección')
+        text.place(x=0, y=210, width=self.w, height=60)
         text.config(font=('Helvetica', 12, 'bold'), justify=tk.CENTER)
         # Images
         lbl1 = tk.Label(self, image=self.img1)
-        lbl1.place(x=50, y=400, width=self.normal_img_size, height=self.normal_img_size)
+        lbl1.place(x=50, y=280, width=self.normal_img_size, height=self.normal_img_size)
         lbl2 = tk.Label(self, image=self.img2)
-        lbl2.place(x=300, y=400, width=self.normal_img_size, height=self.normal_img_size)
+        lbl2.place(x=250, y=280, width=self.normal_img_size, height=self.normal_img_size)
         lbl3 = tk.Label(self, image=self.img3)
-        lbl3.place(x=550, y=400, width=self.normal_img_size, height=self.normal_img_size)
+        lbl3.place(x=450, y=280, width=self.normal_img_size, height=self.normal_img_size)
         lbl3 = tk.Label(self, image=self.img4)
-        lbl3.place(x=800, y=400, width=self.normal_img_size, height=self.normal_img_size)
+        lbl3.place(x=650, y=280, width=self.normal_img_size, height=self.normal_img_size)
         # Buttons
-        btn1 = tk.Button(self, text='GRADIENTES INTEGRADOS', bd=5)
-        btn1.place(x=self.calc_img_x(1), y=620, width=200)
+        btn1 = tk.Button(self, text='GRAD. INTEGRADOS', bd=5)
+        btn1.place(x=50, y=460, width=self.normal_img_size)
         btn1.config(font=('Helvetica', 10), justify=tk.CENTER)
         btn2 = tk.Button(self, text='LIME', bd=5)
-        btn2.place(x=self.calc_img_x(2), y=620, width=200)
+        btn2.place(x=250, y=460, width=self.normal_img_size)
         btn2.config(font=('Helvetica', 10), justify=tk.CENTER)
         btn3 = tk.Button(self, text='XRAI', bd=5)
-        btn3.place(x=self.calc_img_x(3), y=620, width=200)
+        btn3.place(x=450, y=460, width=self.normal_img_size)
         btn3.config(font=('Helvetica', 10), justify=tk.CENTER)
         btn4 = tk.Button(self, text='ANCHOR', bd=5)
-        btn4.place(x=self.calc_img_x(4), y=620, width=200)
+        btn4.place(x=650, y=460, width=self.normal_img_size)
         btn4.config(font=('Helvetica', 10), justify=tk.CENTER)
 
     def load_images_widgets(self, img_name):
+        size_main = (self.main_img_size, self.main_img_size)
+        size_normal = (self.normal_img_size, self.normal_img_size)
         img_name_png = f'{get_name_without_ext(img_name)}.png'
-        self.main_img = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(IMG_FOLDER, img_name)).resize((300, 300)))
-        self.img1 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(IG_FOLDER, img_name_png)).resize((200, 200)))
-        self.img2 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(LIME_FOLDER, img_name_png)).resize((200, 200)))
-        self.img3 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(ANCHOR_FOLDER, img_name_png)).resize((200, 200)))
-        self.img4 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(ANCHOR_FOLDER, img_name_png)).resize((200, 200)))
+        self.main_img = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(IMG_FOLDER, img_name)).resize(size_main))
+        self.img1 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(IG_FOLDER, img_name_png)).resize(size_normal))
+        self.img2 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(LIME_FOLDER, img_name_png)).resize(size_normal))
+        self.img3 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(ANCHOR_FOLDER, img_name_png)).resize(size_normal))
+        self.img4 = ImageTk.PhotoImage(master=self, image=Image.open(os.path.join(ANCHOR_FOLDER, img_name_png)).resize(size_normal))
     
     def calc_img_x(self, img_no):
         return self.space_between + (img_no-1)*(self.normal_img_size + self.space_between)
@@ -138,12 +142,14 @@ class InstructionsWindow(tk.Tk):
     def __init__(self, app):
         super().__init__()
         self.geometry('200x200')
+        self.resizable(False, False)
 
 
 class ThankYouWindow(tk.Tk):
     def __init__(self, app):
         super().__init__()
         self.geometry('100x100')
+        self.resizable(False, False)
 
 
 if __name__ == "__main__":
